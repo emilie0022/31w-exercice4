486216c93283174a6fcf521e42c7e64f767190d4
Émilie Desmarais <emilied2002@gmail.com>
2024-11-17 17:31:51 -0500

    ajout de la fonction charger_scripts_css

--- filtrepost.php
+++ filtrepost.php
@@ -6,4 +6,24 @@ author: Emilie Desmarais
 author uri: https://referenced.ca
 */
 
+function enfile_css_js()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "/style.css");
+    $version_js =  filemtime(plugin_dir_path(__FILE__) . "js/filtrepost.js");
 
+    wp_enqueue_style(
+        "filtrepost",
+        plugin_dir_url(__FILE__) . "/style.css",
+        array(),
+        $version_css
+    );
+    wp_enqueue_script(
+        "filtrepost",
+        plugin_dir_url(__FILE__) . "js/filtrepost.js",
+        array(),
+        $version_js,
+        true
+    );
+}
+
+add_action("wp_enqueue_script", "charger_scripts_css");
